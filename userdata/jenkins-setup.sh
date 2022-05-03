0830aac3cf7d86d4ebaed3be7188b52023c7004d
Andy Nzeka <43660682+andynze@users.noreply.github.com>
2022-05-03 02:34:20 -0500

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install openjdk-8-jdk -y
 sudo apt install ca-certificates -y
-sudo apt install maven git wget unzip -y
+sudo apt install curl maven git wget unzip -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
